6bbd460ce3158c75c9387b4cd96471c7c00da303
nitasha18 <nitashag18@gmail.com>
2020-09-07 19:10:33 +0530

    restructureed the SC and decrease the complexity

--- contracts/CharityDapp.sol
+++ contracts/CharityDapp.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.7.0;
+pragma solidity 0.7.0;
 pragma experimental ABIEncoderV2;
 import './KYC.sol';
 import './CharityInterface.sol';
@@ -9,180 +9,161 @@ import {FaithMiners} from './FaithMinus.sol';
 contract CharityDapp is CharityInterface{
     using SafeMath for uint256;
 
-    address public curator;
     uint256 public charityPoolDonations;
-    
-    constructor() public payable 
-    {
-        curator = msg.sender;   
-    }
+
     
     receive() external payable 
     { 
         charityPoolDonations += msg.value;              //if the contract is called from another contract with amount it will stored in the pool
     }
     
-    struct Proposal {
+    struct Organisation {
+        //about the Organisation/person who have register
+        address payable recipient;                      //address where amount would get transfered
+        string description;                             //brief about Organisation
+    }
+    
+    struct Campaign {
+        //about the proposal input by the user
+        string aboutProposal;                           //description of the proposal
+        bool fullExtraction;                            //whether want to have whole fund out or just as much as raised
+        uint256 fundRaisingDeadline;                    //time till the fund will be open for donating
+        uint256 endTime;                                //endtime for funding completion
+        uint256 campaignStakes;                             //stakes reserved for fund raising
+    }
+    
+    struct CampaignStatus {
         
-        //basic information about the funding
-        address payable recipient;                      //addr where amount will go if proposal is approved 
         uint256 fundingGoal;                            //total amount for raisingthe fund
         uint256 raisedFunds;                             //funds which have been raised for the proposal
         uint256 claimedFunds;                           //funds which are claimed by the user
         
-        string description;                             //description of the proposal
-        
-        uint256 endTime;                                //endtime for funding completion
-        uint256 fundRaisingDeadline;                    //open for plateform users to donate --> period
-        
-        //for checking the availablebility of funding proposal
-        bool fullExtraction;                            //whether want to have whole fund out or just as much as raised
+        //for checking the availability of funding proposal
         bool openForFunding;                            //active or not for funding
-        
-        bool proposalApproved;
-        
-        //staking details of the user from timeAlly
-        uint256 userStakes;                             //stakes reserved for fund raising
-        uint256 delegatedTimePeriod;                    //time period upto which this delegated amount has been staked
-        
-        //For claming the funds
-        uint256 reviewPeriod;                           //time period after how much the person will give the proof of work done
-        uint256 currentReviewCount;                     //number of review period the person has submitted
-        mapping (uint256 => string) reviewedProof;      //proof stored with [amount]->work done (string) :for testing
+        bool proposalApproved;                          //approved by faithminus
     } 
     
-    modifier onlyOwner() {
-        require(msg.sender == curator);
-        _;
-    }
     
-    //temprorary variables for testing purpose
-    uint256 currentTime = 0;
+    uint256 currentTime = 0;                            //temprorary variables for testing purpose 
     
-    // Proposal[] public proposals;
-    mapping (address => Proposal) public proposals;
+    // mapping of all structs
+    mapping (address => Organisation) public organisations;
+    mapping (address => Campaign) public campaigns;
+    mapping (address => CampaignStatus) public campaignStats;
     
     event ProposalAdded(address recipient, uint256 amount, string description);
+    event ProposalAproved(address recipient, uint256 amount, string description);
     event Donated(address proposalAddress, address donorAddress, uint256 amount);
     event ProposalEnded(address recipient,uint256 amount, string description);
     
+    function newOrganisation(string memory _description) public {
+        
+        Organisation storage org = organisations[msg.sender];
+        org.recipient = msg.sender;
+        org.description = _description;
+    }
     
-    function newProposal(uint256 _fundingGoal, string memory _description, uint256 _endTime, uint256 _fundRaisingDeadline, bool _fullExtraction, uint256 _delegatedPeriod, uint256 _reviewPeriod) public
-    {
+    function newCampaign(string memory _aboutProposal, uint256 _fundingGoal, bool _fullExtraction, uint256 _fundRaisingDeadline, uint256 _endTime) public {
+        
         ITimeAllyManager tm;
-        
-        require(_endTime >= (_delegatedPeriod)  , "Deadline for ending the fund is not sufficient since it should also include the claiming period");
         require(tm.isStakingContractValid(msg.sender),"The beneficiery should have stakeing timeAlly contract");
+        Campaign storage cam = campaigns[msg.sender];
+        cam.aboutProposal = _aboutProposal;              
+        setCampaignStatus(_fundingGoal);
+        cam.fullExtraction = _fullExtraction;                       
+        cam.fundRaisingDeadline = _fundRaisingDeadline;   
+        cam.endTime = _endTime;
+        cam.campaignStakes = tm.getTotalActiveStaking(_endTime);  
         
-        Proposal storage p = proposals[msg.sender];
+        emit ProposalAdded(msg.sender,_fundingGoal,_aboutProposal);
+    }
+    
+    function setCampaignStatus(uint _fundingGoal) public
+    {
+        CampaignStatus storage cs = campaignStats[msg.sender];
+                             
+        cs.fundingGoal = _fundingGoal;        
+        cs.raisedFunds = 0;
+        cs.claimedFunds = 0;
+        cs.openForFunding = true;
+        cs.proposalApproved=false;
         
-        p.recipient = msg.sender;
-        p.fundingGoal = _fundingGoal;
-        p.raisedFunds = 0;
-        p.claimedFunds = 0;
-        
-        p.description = _description;
-        
-        p.endTime = _endTime  ;
-        p.fundRaisingDeadline = _fundRaisingDeadline;
-        
-        p.fullExtraction = _fullExtraction;
-        p.openForFunding = true;
-        p.proposalApproved=false;
-        
-        p.userStakes = tm.getTotalActiveStaking(_delegatedPeriod);      //erc173 integration TODO
-        p.delegatedTimePeriod = _delegatedPeriod;
-        p.reviewPeriod = _reviewPeriod;
-        p.currentReviewCount = 0;
-
-        emit ProposalAdded(msg.sender,_fundingGoal,_description);
-        approveProposal(p.recipient);
+        approveProposal(msg.sender);
     }
     
     function approveProposal(address _proposalAddress) internal {
         FaithMiners fm;
-        Proposal storage p = proposals[_proposalAddress];
-        p.proposalApproved = fm.checkProposal(_proposalAddress);
+        CampaignStatus storage cs = campaignStats[_proposalAddress];
+        cs.proposalApproved = fm.checkProposal(_proposalAddress);
+        emit ProposalAproved(_proposalAddress,cs.fundingGoal,campaigns[_proposalAddress].aboutProposal);
     }
     
     function donate(address payable _proposalAddress, uint256 _donateAmount) public onlyDonor returns (bool success)
     {
-        Proposal storage p = proposals[_proposalAddress];
-        // require(d.isRegistered,"The donor is not registered");
-        // require(d.donations > 0,"The donor doesn't have sufficient amount in their wallet");
-        // require(_donateAmount <= d.donations, "The donor dont have suffiecient tokens to donate");
+        CampaignStatus storage cs = campaignStats[msg.sender];
         
-        require(currentTime <= (p.fundRaisingDeadline)  ,"For donating the timelimit has exceeded");
-        require(currentTime <= (p.endTime)  ,"The proposal has reached its deadline");
+        require(cs.openForFunding,"The proposal funding period has been closed");
+        require(_donateAmount <= (cs.fundingGoal-cs.raisedFunds), "The proposal doesn't need that much of donations");
         
-        require(p.openForFunding,"The proposal funding period has been closed");
-        
-        require(_donateAmount <= (p.fundingGoal-p.raisedFunds), "The proposal doesn't need that much of donations");
-        
-        
-        p.raisedFunds += _donateAmount;         //amount would be added to the proposal funds
-        if(p.fundRaisingDeadline <= currentTime || p.raisedFunds == p.fundingGoal)
+        cs.raisedFunds += _donateAmount;         //amount would be added to the proposal funds
+        if(cs.raisedFunds == cs.fundingGoal ||campaigns[_proposalAddress].fundRaisingDeadline <= currentTime)
         {
-            p.openForFunding = false;
+            cs.openForFunding = false;
         }
         emit Donated(_proposalAddress, msg.sender, _donateAmount);
         return true;
     }
     
-    
-    function votingPanel(address _proposalAddress) external onlyFaithminersAndDaogoverners {
-        Proposal storage p = proposals[_proposalAddress];
+    function votingPanel(address _proposalAddress) external onlyFaithminersAndDaogoverners 
+    {
+        Campaign storage cam = campaigns[_proposalAddress];
+        CampaignStatus storage cs = campaignStats[_proposalAddress];
         
-        require(p.fullExtraction==true,"This will only applicable for those proposal who wish to raise full funding goal"); 
-        require(charityPoolDonations > (p.fundingGoal-p.raisedFunds), "The pool should have enough tokens for the proposal");
+        require(cam.fullExtraction==true,"This will only applicable for those proposal who wish to raise full funding goal"); 
+        require(charityPoolDonations > (cs.fundingGoal-cs.raisedFunds), "The pool should have enough tokens for the proposal");
         
-        require(p.proposalApproved,"The proposal is yet to get the approvals");
-        require(!p.openForFunding,"The proposal funding period should be closed");
+        require(cs.proposalApproved,"The proposal is yet to get the approvals");
+        require(!cs.openForFunding,"The proposal funding period should be closed");
         
-        p.raisedFunds = p.fundingGoal;
-        charityPoolDonations -= (p.fundingGoal-p.raisedFunds);
-        p.fundingGoal = 0;
-        p.openForFunding = false;
-        emit Donated(_proposalAddress, curator, (p.fundingGoal - p.raisedFunds));
+        cs.raisedFunds = cs.fundingGoal;
+        charityPoolDonations -= (cs.fundingGoal-cs.raisedFunds);
+        cs.fundingGoal = 0;
+        cs.openForFunding = false;
+        emit Donated(_proposalAddress, msg.sender , (cs.fundingGoal - cs.raisedFunds));
     }
     
-    function claimFunds(address _proposalAddress, string memory _proof) public payable returns (bool) {
+    function claimFunds() public payable returns (bool) {
         
-        Proposal storage p = proposals[_proposalAddress];
+        Organisation storage org = organisations[msg.sender];
+        Campaign storage cam = campaigns[msg.sender];
+        CampaignStatus storage cs = campaignStats[msg.sender];
         
-        require(msg.sender == p.recipient,"Only the recipient is authorised to claimed the funds");
-        require(!p.openForFunding, "The proposal is still up and is going on");
+        require(msg.sender == org.recipient,"Only the recipient is authorised to claimed the funds");
+        require(cs.proposalApproved,"The proposal should be approved");
+        require(!cs.openForFunding, "The proposal is still up and is going on");
+        require(cs.claimedFunds < cs.fundingGoal, "The funds have been already claimed");
         
-        require(p.claimedFunds < p.fundingGoal, "The funds have been already claimed");
-        require(p.proposalApproved,"The proposal should be approved");
-        
-        
-        uint256 numberOfTransactionsToClaimFund = p.fundingGoal/p.userStakes;
-        require(p.currentReviewCount <= numberOfTransactionsToClaimFund, "The review is yet to add");
-        
-        p.reviewedProof[p.currentReviewCount++] = _proof;
-        p.recipient.transfer(p.userStakes); 
-        p.claimedFunds += p.userStakes;
-        if(p.currentReviewCount == numberOfTransactionsToClaimFund || p.claimedFunds == p.raisedFunds)
+        org.recipient.transfer(cam.campaignStakes); 
+        cs.claimedFunds += cam.campaignStakes;
+        if(cs.claimedFunds == cs.raisedFunds)
         {
-            emit ProposalEnded(p.recipient,p.claimedFunds,p.description);
+            emit ProposalEnded(org.recipient,cs.claimedFunds,cam.aboutProposal);
         }
         return true;
     }
   
-    // function getProposal(address _proposalAddress) public view returns (uint256, uint256, uint256, string memory,uint256,uint256,bool,bool,bool, uint256,uint256, uint256,uint256){
-    //     return (proposals[_proposalAddress].fundingGoal,
-    //             proposals[_proposalAddress].raisedFunds,
-    //             proposals[_proposalAddress].claimedFunds,
-    //             proposals[_proposalAddress].description,
-    //             proposals[_proposalAddress].endTime,
-    //             proposals[_proposalAddress].fundRaisingDeadline,
-    //             proposals[_proposalAddress].fullExtraction,
-    //             proposals[_proposalAddress].openForFunding,
-    //             proposals[_proposalAddress].proposalApproved,
-    //             proposals[_proposalAddress].userStakes,
-    //             proposals[_proposalAddress].delegatedTimePeriod,
-    //             proposals[_proposalAddress].reviewPeriod,
-    //             proposals[_proposalAddress].currentReviewCount);
-    // }
+    function getOrganisation(address _proposalAddress) public view returns (string memory){
+        Organisation memory org = organisations[_proposalAddress];
+        return (org.description);
+    }
+    
+    function getCampaign(address _proposalAddress) public view returns (string memory, bool, uint256, uint256, uint256){
+        Campaign memory cam = campaigns[msg.sender];
+        return (cam.aboutProposal,cam.fullExtraction,cam.fundRaisingDeadline, cam.endTime,cam.campaignStakes);
+    }
+    
+    function getCampaignStatus(address _proposalAddress) public view returns (uint256, uint256, uint256, bool, bool){
+        CampaignStatus memory cs = campaignStats[msg.sender];
+        return (cs.fundingGoal,cs.raisedFunds,cs.claimedFunds,cs.openForFunding,cs.proposalApproved);
+    }
 }
--- contracts/FaithMinus.sol
+++ contracts/FaithMinus.sol
@@ -3,13 +3,17 @@ pragma solidity ^0.7.0;
 
 import './CharityDapp.sol';
 
+//logic should be implemented by the FaithMinus
 contract FaithMiners {
     
-    CharityDapp proposal;
+    CharityDapp cd;
 
     uint256 private minProposalStakes = 100;        //min deposit req to submit any proposal
     function checkProposal(address _proposalAddress) public returns (bool){
-        proposal = new CharityDapp();
+        cd = new CharityDapp();
+        // CharityDapp.Campaign memory cam = cd.getCampaign(_proposalAddress);
+        
+        
         // uint256 numberOfTransactionsToClaimFund = proposal.proposals[_proposalAddress].fundingGoal/proposal.proposals(_proposalAddress).userStakes;
         // require(proposal.proposals(_proposalAddress).reviewPeriod > 1,"The review period should be greater than 1 day");
         // require(proposal.proposals(_proposalAddress).delegatedTimePeriod > numberOfTransactionsToClaimFund * proposal.proposals(_proposalAddress).reviewPeriod,"Not enough time is delegated to the stakes for funding in Charity Dapp");
